02e6202314cc86c856cfef44d8308f5821b23075
Shark-Gao <540167318@qq.com>
2026-06-04 22:02:12 +0800

    服务器
    服务器

---
+++ githubsetup.bat
@@ -0,0 +1,7 @@
+# 在本地项目目录中执行
+git init
+git add .
+git commit -m "初始提交：Cocos斗地主游戏服务器"
+git branch -M main
+git remote add origin https://github.com/Shark-Gao/cocos-doudizhu-server
+git push -u origin main
